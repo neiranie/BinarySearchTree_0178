de81f4496731f1d1a9999eb798abc265b81b3641
neiranie <nurkhairanianneira@gmail.com>
2025-06-26 10:22:16 +0700

    adding step 6 and 7

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -57,6 +57,23 @@ BinaryTree()
             return;
         }
 
-        
+        // Step 6: If the value in the data field of new node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a: Make the left child of parent point to the new node
+            parent->leftchild = newNode;
+
+            // 6b: Exit
+            return;
+        }
+        // Step 7: If the value in the data field of the new node is greater than that of the parent
+        else if (x > parent->info)
+        {
+            // 7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            // 7b: Exit
+            return;
+        }
     }
 };
