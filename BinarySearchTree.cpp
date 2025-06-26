45567b03b9566bf55a27ef4737eafb302f78c9ce
neiranie <nurkhairanianneira@gmail.com>
2025-06-26 10:09:08 +0700

    adding step 1 - 3

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -34,6 +34,13 @@ BinaryTree()
 
         // Step 1: Allocate memory for the new node
         Node *newNode = new Node();
+
+        // Step 2: Assign value to the data field of new node
+        newNode->info = x;
+
+        // Step 3: Make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
     }
 
-}
+};
