bab83dbe5315e6040d0e28a32fb17d8fcf2eb071
neiranie <nurkhairanianneira@gmail.com>
2025-06-26 10:02:33 +0700

    adding constructor for the node class

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -7,3 +7,11 @@ public:
     int info;
     Node *leftchild;
     Node *rightchild;
+
+     //Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // Initialize left child to null
+        rightchild = nullptr; // Initialize right child to null
+    }
+};
