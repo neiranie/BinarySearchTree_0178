aefac48aee47525b155c67596af40cde9c3c49f5
neiranie <nurkhairanianneira@gmail.com>
2025-06-26 10:45:34 +0700

    adding void postorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -121,5 +121,19 @@ BinaryTree()
         preorder(ptr->leftchild);
         preorder(ptr->rightchild);
     }
-    
+
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+        
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; 
+    }
 };
